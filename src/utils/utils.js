294c6c3b02eb1e561581db95d844b579aa79ac99
andrew <asc269@cornell.edu>
2023-02-03 20:18:15 -0500

    Small changes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,8 @@
 export const commonQuestions = [
   'What is the purpose of the document and does it serve that purpose effectively?',
-  'Does the document comply with the jurisdiction in which it will be used?',
-  'Does the document have the proper legal authority to support its provisions?',
-  'Are the facts and information presented in the document accurate and up-to-date?',
-  'Does the document comply with all relevant laws, regulations, and industry standards?',
-  'Are the provisions in the document enforceable and legally binding?',
-  'Does the document remain consistent throughout and avoid any conflicting provisions?',
-  'Does the document contain any ambiguous or unclear language that could be interpreted differently?',
+  // 'Are the provisions in the document enforceable and legally binding?',
+  // 'Does the document remain consistent throughout and avoid any conflicting provisions?',
+  // 'Does the document contain any ambiguous or unclear language that could be interpreted differently?',
 ];
 
 export default async function cuad({ question, context }) {
